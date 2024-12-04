ec95d58a20220ab5d3a1450511c4bdbc33736661
msebaf <msebaf@hotmail.com>
2024-12-04 19:54:35 -0300

    bakcup previo a recycler view casos

---
+++ HC_digital/app/src/main/java/com/msf/HC_digital/ui/asentarIntervencion/AsentarIntervencionViewModel.java
@@ -0,0 +1,7 @@
+package com.msf.HC_digital.ui.asentarIntervencion;
+
+import androidx.lifecycle.ViewModel;
+
+public class AsentarIntervencionViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
